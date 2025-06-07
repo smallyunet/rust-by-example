e779c0623a6af1bafcb1ae2d4c99a7c00b8af0c0
smallyunet <dev.wangyu@gmail.com>
2025-06-07 10:29:05 +0800

    0300
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

---
+++ 0300-comcepts/demo/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    // let x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+    // let mut x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+    println!("The value of x is: {}", x);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("The length of spaces is: {}", spaces);
+
+    let mut spaces = "    ";
+    spaces = spaces.len().to_string();
+    println!("The length of spaces is: {}", spaces);
+}
